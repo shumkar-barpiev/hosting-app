67816b410f459056d8d66a75e7503abd00689917
Shumkarbek Barpyev <barpievsh@gmail.com>
2025-07-03 13:25:57 +0600

    Welcome to Material UI with Next.js!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+
+export default function Home() {
+  return (
+    <main>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Welcome to Material UI with Next.js!
+      </Typography>
+    </main>
+  );
 }
